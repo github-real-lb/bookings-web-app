988e846d379e3ba32285e85ac04ad7db138c9c0c
Liron Birenboum <code-real-lb@pm.me>
2024-03-19 17:38:47 -0500

    adding handlers testings for get method

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/github-real-lb/bookings-web-app/internal/forms"
+	"github.com/github-real-lb/bookings-web-app/internal/models"
+	"github.com/github-real-lb/bookings-web-app/internal/render"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+type Store struct{}
+
 type Server struct {
 	http.Server
+	Store
 }
 
-// NewServer returns New http.Server to serves all HTTP requests
+// NewServer returns a new http.Server with all routings and handlers
 func NewServer(addr string) *Server {
-	mux := chi.NewRouter()
 	server := Server{
 		Server: http.Server{
 			Addr:    addr,
-			Handler: mux,
+			Handler: NewHandler(Store{}),
 		},
 	}
+
+	return &server
+}
+
+// NewHandler returns a new http.Handler with all routings
+func NewHandler(store Store) http.Handler {
+	mux := chi.NewRouter()
 
 	// add middleware that recover from panics
 	mux.Use(middleware.Recoverer)
@@ -31,28 +47,161 @@ func NewServer(addr string) *Server {
 	mux.Use(NoSurf)
 
 	// setting routes
-	mux.Get("/", server.Home)
-	mux.Get("/about", server.About)
+	mux.Get("/", store.Home)
+	mux.Get("/about", store.About)
 
-	mux.Get("/generals-quarters", server.Generals)
-	mux.Post("/generals-quarters", server.PostAvailability)
+	mux.Get("/generals-quarters", store.Generals)
+	mux.Post("/generals-quarters", store.PostAvailability)
 
-	mux.Get("/majors-suite", server.Majors)
-	mux.Post("/majors-suite", server.PostAvailability)
+	mux.Get("/majors-suite", store.Majors)
+	mux.Post("/majors-suite", store.PostAvailability)
 
-	mux.Get("/contact", server.Contact)
+	mux.Get("/contact", store.Contact)
 
-	mux.Get("/search-availability", server.Availability)
-	mux.Post("/search-availability", server.PostAvailability)
-	mux.Post("/search-availability-json", server.AvailabilityJSON)
+	mux.Get("/search-availability", store.Availability)
+	mux.Post("/search-availability", store.PostAvailability)
+	mux.Post("/search-availability-json", store.AvailabilityJSON)
 
-	mux.Get("/make-reservation", server.Reservation)
-	mux.Post("/make-reservation", server.PostReservation)
-	mux.Get("/reservation-summary", server.ReservationSummary)
+	mux.Get("/make-reservation", store.Reservation)
+	mux.Post("/make-reservation", store.PostReservation)
+	mux.Get("/reservation-summary", store.ReservationSummary)
 
 	// setting file server
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
-	return &server
+	return mux
+}
+
+// Home is the home page handler
+func (s *Store) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "home.page.gohtml", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (s *Store) About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "about.page.gohtml", &models.TemplateData{})
+}
+
+// Reservation is the generals-quarters room page handler
+func (s *Store) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "generals.room.page.gohtml", &models.TemplateData{})
+}
+
+// Majors is the majors-suite room page handler
+func (s *Store) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "majors.room.page.gohtml", &models.TemplateData{})
+}
+
+// Contact is the contact page handler
+func (s *Store) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "contact.page.gohtml", &models.TemplateData{})
+}
+
+// Availability is the search-availability page handler
+func (s *Store) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "search-availability.page.gohtml", &models.TemplateData{})
+}
+
+// PostAvailability is the search-availability page post handler
+func (s *Store) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	startDate := r.Form.Get("start_date")
+	endDate := r.Form.Get("end_date")
+
+	w.Write([]byte(fmt.Sprintf("Start date is %s and end date is %s", startDate, endDate)))
+}
+
+type jsonResponse struct {
+	StartDate time.Time `json:"start_date"`
+	EndDate   time.Time `json:"end_date"`
+}
+
+// AvailabilityJSON handles requests for availability and sends JSON response
+func (s *Store) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+
+	startDate, err := time.Parse("2006-01-02", r.Form.Get("start_date"))
+	if err != nil {
+		log.Println(err)
+	}
+	endDate, err := time.Parse("2006-01-02", r.Form.Get("end_date"))
+	if err != nil {
+		log.Println(err)
+	}
+
+	resp := jsonResponse{
+		StartDate: startDate,
+		EndDate:   endDate,
+	}
+
+	out, err := json.Marshal(resp)
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+// Reservation is the make-reservation page handler
+func (s *Store) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "make-reservation.page.gohtml", &models.TemplateData{
+		Form: forms.New(nil),
+		Data: map[string]any{
+			"reservation": models.Reservation{},
+		},
+	})
+}
+
+// PostReservation is the make-reservation post page handler
+func (s *Store) PostReservation(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Email:     r.Form.Get("email"),
+		Phone:     r.Form.Get("phone"),
+	}
+
+	form := forms.New(r.PostForm)
+	form.Required("first_name", "last_name", "email")
+	form.MinLenght("first_name", 3)
+	form.MinLenght("last_name", 3)
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		render.RenderTemplate(w, r, "make-reservation.page.gohtml", &models.TemplateData{
+			Form: form,
+			Data: map[string]any{
+				"reservation": reservation,
+			},
+		})
+
+		return
+	}
+
+	app.Session.Put(r.Context(), "reservation", reservation)
+
+	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
+}
+
+func (s *Store) ReservationSummary(w http.ResponseWriter, r *http.Request) {
+	reservation, ok := app.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		log.Println("cannot get reservation from the session.")
+		app.Session.Put(r.Context(), "error", "No reservation exists. Please make a reservation.")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	app.Session.Remove(r.Context(), "reservation")
+
+	render.RenderTemplate(w, r, "reservation-summary.page.gohtml", &models.TemplateData{
+		Data: map[string]any{
+			"reservation": reservation,
+		},
+	})
 }
--- cmd/web/server_test.go
+++ cmd/web/server_test.go
@@ -1,12 +1,63 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewServer(t *testing.T) {
 	server := NewServer(ADDRESS)
 	assert.IsType(t, (*Server)(nil), server)
+}
+
+func TestNewHandler(t *testing.T) {
+	handler := NewHandler(Store{})
+	assert.Implements(t, (*http.Handler)(nil), handler)
+}
+
+func TestPageHandlers(t *testing.T) {
+	tests := []struct {
+		name   string // name of test
+		method string // http.Method for the http.Request
+		url    string // url for the http.Request
+		params []struct {
+			key   string
+			value string
+		}
+		excpectedStatusCode int
+	}{
+		{"Home", http.MethodGet, "/", nil, http.StatusOK},
+		{"About", http.MethodGet, "/about", nil, http.StatusOK},
+		{"Generals", http.MethodGet, "/generals-quarters", nil, http.StatusOK},
+		{"Majors", http.MethodGet, "/majors-suite", nil, http.StatusOK},
+		{"Contact", http.MethodGet, "/contact", nil, http.StatusOK},
+		{"Availability", http.MethodGet, "/search-availability", nil, http.StatusOK},
+		{"Reservation", http.MethodGet, "/make-reservation", nil, http.StatusOK},
+	}
+
+	// initialize the Application Config and Templates
+	templatePath = "./../../templates"
+	InitApp()
+
+	// start test server and send request
+	testServer := httptest.NewTLSServer(NewHandler(Store{}))
+	defer testServer.Close()
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.method == http.MethodGet {
+				response, err := testServer.Client().Get(testServer.URL + test.url)
+				require.NoError(t, err)
+
+				assert.Equal(t, test.excpectedStatusCode, response.StatusCode)
+			} else {
+				// TODO
+			}
+
+		})
+	}
 }
