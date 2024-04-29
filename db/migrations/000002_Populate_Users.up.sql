00e42329c707e49968e204b2c950af8b04592a5e
Liron Birenboum <code-real-lb@pm.me>
2024-04-28 23:31:46 -0400

    adding 50 mock reservations to migration for testing revising the base layout of base public site revising the base layout of base admin dashboard

---
+++ db/migrations/000002_Populate_Users.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (
+  first_name, last_name, email, password, access_level
+) VALUES (
+  'admin', 'admin', 'admin@admin.com', '$2a$10$jLfV9aSVfpP8uPuf9.U4WOc6S7unmOxQxNBSRMIMvdVryTCdxvqEO', 1
+)
--- db/migrations/000003_Populate_Rooms.up.sql
+++ db/migrations/000003_Populate_Rooms.up.sql
@@ -49,9 +49,3 @@ INSERT INTO rooms (
   Guests can unwind in the lavish surroundings or explore nearby attractions, knowing that every comfort is catered to their needs.',
   'admirals-haven.png'
 );
-
-INSERT INTO users (
-  first_name, last_name, email, password, access_level
-) VALUES (
-  'admin', 'admin', 'admin@admin.com', '$2a$10$jLfV9aSVfpP8uPuf9.U4WOc6S7unmOxQxNBSRMIMvdVryTCdxvqEO', 1
-)
